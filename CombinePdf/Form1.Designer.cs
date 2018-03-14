4a0e3299abec27553a350e3b17d08f5273a31e2c
Felipe Trevizan CPQi <ftrevizan@cpqi.com>
2018-03-14 13:39:20 -0300

    Primeira subida.

---
+++ CombinePdf/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace CombinePdf
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCombinar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCombinar
+            // 
+            this.btnCombinar.Location = new System.Drawing.Point(70, 81);
+            this.btnCombinar.Name = "btnCombinar";
+            this.btnCombinar.Size = new System.Drawing.Size(75, 23);
+            this.btnCombinar.TabIndex = 0;
+            this.btnCombinar.Text = "Combinar";
+            this.btnCombinar.UseVisualStyleBackColor = true;
+            this.btnCombinar.Click += new System.EventHandler(this.btnCombinar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnCombinar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCombinar;
+    }
+}
+
---
+++ CombinePdf/Form1.cs
@@ -0,0 +1,51 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CombinePdf
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCombinar_Click(object sender, EventArgs e)
+        {
+            Document document = new Document();
+
+            PdfCopy writer = new PdfCopy(document, new FileStream(@"C:\Users\agaspari\Documents\Temp\telecurso\matematica_1.pdf", FileMode.Create));
+            if (writer == null)
+                return;
+
+            document.Open();
+
+            foreach(string fileName in Directory.GetFiles(@"C:\Users\agaspari\Documents\Temp\telecurso\matematica_1", "*.pdf", SearchOption.TopDirectoryOnly))
+            {
+                PdfReader reader = new PdfReader(fileName);
+                reader.ConsolidateNamedDestinations();
+
+                for (int i = 1; i <= reader.NumberOfPages; i++)
+                {
+                    PdfImportedPage page = writer.GetImportedPage(reader, i);
+                    writer.AddPage(page);
+                }
+
+                reader.Close();
+            }
+
+            writer.Close();
+            document.Close();
+        }
+    }
+}
